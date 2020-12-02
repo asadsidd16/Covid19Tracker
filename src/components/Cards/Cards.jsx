a26f684e4ac229b9fc1ff40685e7df3a13dd5b2d
asadsidd16 <asadsidd16@csu.fullerton.edu>
2020-12-01 19:59:57 -0800

    Added buttons in cards components

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,6 +3,7 @@ import {Card, CardContent, Typography, Grid } from '@material-ui/core';
 import CountUp from 'react-countup';
 import styles from './Cards.module.css';
 import cx from 'classnames';
+import Button from '@material-ui/core/Button';
 
 const Cards = ( {data:{confirmed, recovered, deaths, lastUpdate} } ) => {
     
@@ -59,10 +60,23 @@ const Cards = ( {data:{confirmed, recovered, deaths, lastUpdate} } ) => {
                             Number of death caused by COVID-19
                         </Typography>
                     </CardContent>
+                    </Grid>
+                    <Grid className={styles.btnGrid} >   
+                        <Button  style={{backgroundColor: '#4A148C'}} className={styles.btnMyGov}  variant="contained" color="primary" href="https://www.mohfw.gov.in/">  
+                            mohfw  
+                        </Button>   
+                        <Button style={{backgroundColor: '#004D40'}} className={styles.btnMyGov}  variant="contained" color="primary" href="https://www.mygov.in/covid-19">  
+                        mygov  
+                        </Button>  
+                        <Button style={{backgroundColor: '#900C3F'}} className={styles.btnMyGov}  variant="contained" color="primary" href="https://twitter.com/who?lang=en">  
+                        WHO  
+                        </Button>  
+                        <Button style={{backgroundColor: '#581845'}} className={styles.btnMyGov}  variant="contained" color="primary" href="https://twitter.com/PIB_India">  
+                        PIB:IN  
+                       </Button> 
                 </Grid>
             </Grid>
         </div>
     )
 }
-
 export default Cards;
